57ea6e0726694ac24ad8bf868958adaaf5bfdea9
Elvir Misini <58117020+elvirmisini@users.noreply.github.com>
2021-07-06 20:55:02 +0200

    Add files via upload

---
+++ auth/api/backend.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import { v4 as uuidv4 } from 'uuid'
+
+export const sendData = (fullName, email) => {
+  const headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+  }
+
+  const role = checkRole(email)
+  const id = uuidv4()
+
+  const userObj = {
+    id,
+    fullName,
+    role,
+    email,
+  }
+  const url = `${process.env.REACT_APP_WEBAPI}/User/Add`
+
+  axios.post(url, userObj, headers).then((res) => {
+    console.log(res)
+    localStorage.setItem('id', res.data)
+  })
+  localStorage.setItem('role', role)
+}
+
+const checkRole = (email) => {
+  // eslint-disable-next-line
+  const [_, domain] = email.split('@')
+
+  if (domain === 'student.uni-pr.edu') return 2
+  return 1
+}
