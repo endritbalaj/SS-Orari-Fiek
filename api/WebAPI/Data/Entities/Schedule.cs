af5da99ae4904cbe3326313a7979326778965654
endrit <58190531+endritbalaj@users.noreply.github.com>
2021-07-08 12:17:15 -0700

    Entities

---
+++ api/WebAPI/Data/Entities/Schedule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Data.Entities
+{
+    public class Schedule
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public Guid Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string Date { get; set; }
+        
+        public string StartTime { get; set; }
+
+        public string EndTime { get; set; }
+        
+        public bool Repeatable { get; set; }
+        
+        public ICollection<User> Students { get; set; }
+        
+        public Guid ProfessorId { get; set; }
+        
+        public Guid SubjectId { get; set; }
+        
+        public Subject Subject { get; set; }
+        
+        public int Year { get; set; }
+    }
+}
