ece4eb972d91dbc8874015b24583ade21735afbe
endrit <58190531+endritbalaj@users.noreply.github.com>
2021-07-08 12:21:31 -0700

    Study year

---
+++ api/WebAPI/Data/Entities/StudyYear.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Data.Entities
+{
+    public class StudyYear
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public Guid Id { get; set; }
+        
+        public Guid StudentId { get; set; }
+        
+        public int YearOfStudy { get; set; }
+    }
+}
