12668990aef1351b91b83ac1eff05b543053bc0b
endrit <58190531+endritbalaj@users.noreply.github.com>
2021-07-08 12:16:08 -0700

    AppDbContext

---
+++ api/WebAPI/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Data.Entities;
+
+namespace WebAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        
+        public DbSet<Schedule> Schedules { get; set; }
+        
+        public DbSet<Subject> Subjects { get; set; }
+        
+        public DbSet<StudyYear> StudyYears { get; set; }
+        
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
