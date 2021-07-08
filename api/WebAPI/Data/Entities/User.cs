d277db94ead5615096d3ff79f985cb4c9bf0cdb2
endrit <58190531+endritbalaj@users.noreply.github.com>
2021-07-08 12:20:04 -0700

    User entities

---
+++ api/WebAPI/Data/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using WebAPI.Enums;
+
+namespace WebAPI.Data.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        
+        public string FullName { get; set; }
+        
+        public string Email { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public Roles Role { get; set; }
+        
+        public virtual ICollection<Subject> Subjects { get; set; }
+        
+        public virtual ICollection<Schedule> Schedules { get; set; }
+    }
+}
