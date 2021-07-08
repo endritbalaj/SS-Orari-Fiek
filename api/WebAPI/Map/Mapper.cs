377b343740f0e5b540cdd676018a6a81394134ba
endrit <58190531+endritbalaj@users.noreply.github.com>
2021-07-08 12:26:20 -0700

    Add files via upload

---
+++ api/WebAPI/Map/Mapper.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using WebAPI.Data.Entities;
+using WebAPI.Models;
+
+namespace WebAPI.Map
+{
+    public class Mapper : Profile
+    {
+        public Mapper()
+        {
+            CreateMap<UserModel, User>()
+                .ForMember(x => x.Schedules, opt => opt.Ignore())
+                .ForMember(x => x.Subjects, opt => opt.Ignore());
+
+            CreateMap<SubjectModel, Subject>()
+                .ForMember(x => x.Students, opt => opt.Ignore());
+            
+            CreateMap<ScheduleModel, Schedule>()
+                .ForMember(x => x.Students, opt => opt.Ignore());
+        }
+    }
+}
